bfe2cbd3145512ca32ce8afc4cc9cdb965d62a84
nima-sh <nima.shayani1379@gmail.com>
2022-07-03 23:45:40 +0430

    sharti

---
+++ index.js
@@ -0,0 +1,8 @@
+const userAge = +prompt('inter your age')
+const userAgeOld = 56 + userAge
+if (userAge >= 18) {
+    if (userAge <= 20) {
+        console.log('right age')
+        console.log(userAgeOld)
+    }
+}
